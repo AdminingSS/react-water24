35c47d542a2f6d979f83f288df030e4f3139eee7
AdminingSS <admining@mail.ru>
2020-05-06 15:48:34 +0300

    map added

--- src/components/MapBody.js
+++ src/components/MapBody.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import GoogleMapReact from 'google-map-react';
 
 class MapBody extends Component {
+
+    state = {
+        geocoder: null,
+        map: null,
+        infoWindow: null,
+        apiLoaded: false,
+        address: ''
+    };
+
     render() {
         return (
             <div>
@@ -16,19 +26,88 @@ class MapBody extends Component {
                     <div className="uk-container">
                         <div className="tm-map-form">
                             <div className="uk-width-1-1">
-                                <input className="uk-input" type="text" placeholder="Введите ваш город" />
+                                <input className="uk-input" type="text" placeholder="Введите ваш город" value={this.state.address} onChange={this.handleInput}/>
                             </div>
-                            <button className="uk-button uk-button-primary">Найти</button>
+                            <button className="uk-button uk-button-primary" onClick={this.geocodeAddress}>Найти</button>
                         </div>
                     </div>
                 </div>
 
-                <div className="tm-map-holder" id="map">
-
+                <div className="tm-map-holder" style={{height: '280px'}} id="map">
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: 'AIzaSyDsqXUXTRCHkVoWGioOxGr6LoXO8VZwpJs'}}
+                    defaultCenter={{lat: 55.5815244, lng: 36.8251017}}
+                    defaultZoom={14}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({map, maps}) => this.handleApiLoaded(map, maps)}
+                />
                 </div>
 
             </div>
         );
+    };
+
+    handleInput = e => {
+        this.setState({address: e.target.value})
+    };
+
+    handleApiLoaded = (map, maps) => {
+// The location of Moscow
+
+        var geocoder = new maps.Geocoder;
+        var infoWindow = new maps.InfoWindow;
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                infoWindow.open(map);
+                map.setCenter(pos);
+            }, function() {
+                //handle locerror
+            });
+        }
+
+        // The marker, positioned at Moscow
+        const marker = new maps.Marker({position: map.getCenter(), map: map});
+
+        this.setState({
+            geocoder: geocoder,
+            map: map,
+            infoWindow: infoWindow,
+            maps: maps,
+            apiLoaded: true
+        });
+    };
+
+    geocodeAddress = e => {
+        const {geocoder, map, infoWindow, maps, apiLoaded, address} = this.state;
+
+        if(!apiLoaded) return;
+
+        geocoder.geocode({'address': address}, function(results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    map.setZoom(15);
+                    map.setCenter(results[0].geometry.location);
+                    const marker = new maps.Marker({
+                        map: map,
+                        position: results[0].geometry.location
+                    });
+                    infoWindow.setContent(results[0].formatted_address);
+                    infoWindow.open(map, marker);
+                } else {
+                    window.alert('No results found');
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+        });
     }
 }
 
